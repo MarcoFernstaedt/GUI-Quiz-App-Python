dec962498643605a48ba4165c8c7129c1b015f6c
Marco <fernstaedtmarco@gmail.com>
2022-10-22 14:50:45 -0700

    finished quiz app UI

--- ui.py
+++ ui.py
@@ -1,2 +1,35 @@
+from tkinter import *
+
+from numpy import imag
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR,)
+        
+        self.score_label = Label(text="Score: 0", fg="White", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150, 125, 
+            text="Some question text", 
+            fill=THEME_COLOR, 
+            font=("Arial", 28, "italic")
+        )
+        
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
